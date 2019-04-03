bfda8a373a381fa2bff59b57e695dc0648ef1f02
Mojo Talantikite <mojo.talantikite@gmail.com>
2019-04-03 18:36:50 -0400

    Add nonce test

--- akamai/signer_test.go
+++ akamai/signer_test.go
@@ -7,11 +7,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testBuildTime(t *testing.T) {
+func TestBuildTime(t *testing.T) {
 	var ctx signingCtx
 	ctx.buildTime()
+	time := ctx.formattedTime
 	expected := regexp.MustCompile(`^\d{4}[0-1][0-9][0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\+0000$`)
-	if assert.Regexp(t, expected, ctx.buildTime, "Fail: Regex do not match") {
+	if assert.Regexp(t, expected, time, "Fail: Regex do not match") {
 		t.Log("Pass: Regex matches")
+	}
+}
+
+func TestBuildNonce(t *testing.T) {
+	var ctx signingCtx
+	ctx.buildNonce()
+	first := ctx.nonce
+	for i := 0; i < 100; i++ {
+		ctx.buildNonce()
+		second := ctx.nonce
+		assert.NotEqual(t, first, second, "Fail: Nonce matches")
 	}
 }
